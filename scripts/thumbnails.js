79010483b49e9e4e1d7956bde6982f4b07f76b9b
jcooke1986uk <jcooke.web@gmail.com>
2018-01-16 21:27:51 +0000

    ajax details fetch
    Using an ajax call to fetch the data from the php file.

--- scripts/thumbnails.js
+++ scripts/thumbnails.js
@@ -16,5 +16,50 @@ function initPage() {
 			getDetails(this.title);
 		}
 	}
+}
 
+function createRequest () {
+	try {
+		request = new XMLHttpRequest();
+	}
+	catch (tryMS) {
+		try {
+			request = new ActiveXObject("Msxm12.XMLHTTP");
+		}
+		catch (otherMS) {
+			try {
+				request = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			catch (failed) {
+				request = null;
+			}
+		}
+	}
+	return request;
+}
+
+function getDetails(itemName) {
+	request = createRequest();
+
+	if (request==null) {
+		alert("Unable to create request");
+		return;
+	}
+
+	var url = "getDetails.php?ImageID=" + escape(itemName);
+
+	request.open("GET", url, true);
+
+	request.onreadystatechange = displayDetails;
+
+	request.send(null);
+}
+
+function displayDetails() {
+	if (request.readyState == 4) {
+		if (request.status == 200) {
+			detailDiv = document.getElementById("description");
+			detailDiv.innerHTML = request.responseText;
+		}
+	}
 }
